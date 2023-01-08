0a9b4a3bc71670bd36e49494681e037486b9e618
conatsu <conachan1125@gmail.com>
2023-01-08 18:00:25 +0900

    first commit

---
+++ queries/queries.ts
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client'
+
+export const GET_USERS = gql`
+  query GetUsers {
+    users(order_by: {created_at: desc}) {
+      id
+      name
+      created_at
+    }
+  }
+`
+
+export const GET_USERS_LOCAL = gql`
+  query GetUsers {
+    users(order_by: {created_at: desc}) @client {
+      id
+      name
+      created_at
+    }
+  }
+`
+
+export const GET_USERIDS = gql`
+  query GetUsersIds {
+    users(order_by: {created_at: desc}) {
+      id
+    }
+  }
+`
+
+export const GET_USERBY_ID = gql`
+  query GetUserById($id: uuid!) {
+    users_by_pk(id: $id) {
+      id
+      name
+      created_at
+    }
+  }
+`
