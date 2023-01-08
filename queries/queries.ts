e99d7974a0ce838a9e6bb0859db4dcc83399c81b
conatsu <conachan1125@gmail.com>
2023-01-08 22:41:59 +0900

    make types

--- queries/queries.ts
+++ queries/queries.ts
@@ -37,3 +37,33 @@ export const GET_USERBY_ID = gql`
     }
   }
 `
+
+export const CREATE_USER = gql`
+  mutation CreateUser($name: String!) {
+    insert_users_one(object: {name: $name}) {
+      id
+      name
+      created_at
+    }
+  }
+`
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: uuid!) {
+    delete_users_by_pk(id: $id) {
+      id
+      name
+      created_at
+    }
+  }
+`
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser($id: uuid!, $name: String!) {
+    update_users_by_pk(pk_columns: {id: $id}, _set: {name: $name}) {
+      id
+      name
+      created_at
+    }
+  }
+`
